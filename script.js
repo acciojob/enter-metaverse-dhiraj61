54f54e1230c8b59a1e080da87441ee3c2862508f
Dhiraj Chaudhari <141726259+dhiraj61@users.noreply.github.com>
2026-04-16 09:25:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,6 @@
 let content = document.getElementById("meta");
 let btn = document.getElementById("enter");
 btn.addEventListener('click',()=>{
+	content.textContent = "Entered Metaverse";
 	content.classList.add("highlight");
 })
