58a9e69d01d5d99ede97fdfbac0c512e1ef989a1
Dhiraj Chaudhari <141726259+dhiraj61@users.noreply.github.com>
2026-04-16 09:25:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let content = document.getElementById("meta");
+let btn = document.getElementById("enter");
+btn.addEventListener('click',()=>{
+	content.classList.add("highlight");
+})
