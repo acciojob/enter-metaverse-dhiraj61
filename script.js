f310a097478f17ec1337d01cfecfc8de9d1dbdaf
Dhiraj Chaudhari <141726259+dhiraj61@users.noreply.github.com>
2026-04-16 09:45:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
-let content = document.getElementById("meta");
 let btn = document.getElementById("enter");
+
 btn.addEventListener('click', () => {
-	content.outerHTML = "<h1 id='meta'>Entered Metaverse</h1>";
+	document.getElementById("meta").outerHTML = "<h1 id='meta'>Entered Metaverse</h1>";
 });
